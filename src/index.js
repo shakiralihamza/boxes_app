7cc999a8f0db235291d7403a76b61847e584c317
shakiralihamza <33418660+shakiralihamza@users.noreply.github.com>
2020-06-27 09:10:00 +0500

    Converted string update to number (type casted). Now it's working fine. Added some cool styling as well. I was comparing strings instead of numbers lol.

--- src/index.js
+++ src/index.js
@@ -6,20 +6,50 @@ import './index.css';
 function App() {
     return (
         <>
-            <Dimensions/>
+            <center>
+                <Dimensions/>
+            </center>
         </>
     );
 }
 function Dimensions() {
-    let [box1_width, set_box1_width] = useState(20);
-    let [box1_height, set_box1_height] = useState(20);
-    let [box2_width, set_box2_width] = useState(20);
-    let [box2_height, set_box2_height] = useState(20);
+    let [box1_width, set_box1_width] = useState(50);
+    let [box1_height, set_box1_height] = useState(50);
+    let [box2_width, set_box2_width] = useState(50);
+    let [box2_height, set_box2_height] = useState(50);
 
-    const set_box1_width_func = event => set_box1_width(box1_width = event.target.value);
-    const set_box1_height_func = event => set_box1_height(box1_height = event.target.value);
-    const set_box2_width_func = event => set_box2_width(box2_width = event.target.value);
-    const set_box2_height_func = event => set_box2_height(set_box2_height = event.target.value);
+    const set_box1_width_func = event => {
+        let input_value = event.target.value;
+        if (Number(input_value) || input_value === '') {
+            return (
+                set_box1_width(box1_width = Number(event.target.value))
+            );
+        }
+    }
+    const set_box1_height_func = event => {
+        let input_value = event.target.value;
+        if (Number(input_value) || input_value === '') {
+            return (
+                set_box1_height(box1_height = Number(event.target.value))
+            );
+        }
+    }
+    const set_box2_width_func = event => {
+        let input_value = event.target.value;
+        if (Number(input_value) || input_value === '') {
+            return (
+                set_box2_width(box2_width = Number(event.target.value))
+            );
+        }
+    }
+    const set_box2_height_func = event => {
+        let input_value = event.target.value;
+        if (Number(input_value) || input_value === '') {
+            return (
+                set_box2_height(set_box2_height = Number(event.target.value))
+            );
+        }
+    }
 
     return (
         <>
@@ -27,22 +57,22 @@ function Dimensions() {
             <p><span style={{color: 'red'}}>Box 1</span> Dimensions:</p>
             <label>
                 Width:
-                <input onClick={(e) => e.target.value = ''} className={"box1_input"} value={box1_width} onChange={set_box1_width_func} type="number"/>
+                <input className={"box1_input"} value={box1_width} onChange={set_box1_width_func} type="text"/>
             </label>
             <label>
                 Height:
-                <input onClick={(e) => e.target.value = ''} className={"box1_input"} value={box1_height} onChange={set_box1_height_func} type="number"/>
+                <input className={"box1_input"} value={box1_height} onChange={set_box1_height_func} type="text"/>
             </label>
 
             {/*Box two:*/}
             <p><span style={{color: 'blueviolet'}}>Box 2</span> Dimensions:</p>
             <label>
                 Width:
-                <input onClick={(e) => e.target.value = ''} className={"box2_input"} value={box2_width} onChange={set_box2_width_func} type="number"/>
+                <input className={"box2_input"} value={box2_width} onChange={set_box2_width_func} type="text"/>
             </label>
             <label>
                 Height:
-                <input onClick={(e) => e.target.value = ''} className={"box2_input"} value={box2_height} onChange={set_box2_height_func} type="number"/>
+                <input className={"box2_input"} value={box2_height} onChange={set_box2_height_func} type="text"/>
             </label>
             <center>
                 <div style={{display: 'inline-block'}}>
@@ -66,23 +96,20 @@ function Boxes({width1, height1, width2, height2}) {
     );
 }
 function CanItHold({width1, height1, width2, height2}) {
-    let result;
+    let result = "Nope, neither of the boxes can hold the other. Try changing dimensions.";
     let big_box = 0;
     if (width1 > 0 && width2 > 0 && height1 > 0 && height2 > 0) {
         if((height1 > height2)) {
             if ((width1 > width2)) {
-                result = "Box 1 can hold box 2, as shown below: ";
+                result = "Yes, Box 1 can hold Box 2, as shown below: ";
                 big_box = 1;
             }
         }
         else if((height1 < height2)) {
             if ((width1 < width2)) {
-                result = "Box 2 can hold box 1, as shown below:";
+                result = "Yes, Box 2 can hold Box 1, as shown below:";
                 big_box = 2;
             }
-        }
-        else {
-            result = "Neither of the boxes can hold the other.";
         }
     }
     return (
@@ -90,17 +117,19 @@ function CanItHold({width1, height1, width2, height2}) {
             {width1&&width2?<p>Now, the question is, can one box hold another?</p>:''}
             {width1&&width2?<p>Let's calculate:</p>:''}
             <br/>
-            {width1&&width2?result:''}
+            {result}
             <br/>
             <br/>
-            {big_box === 1?<div className={"box1_out"} style={{width:width1+'px', height:height1+'px'}}>
-                <div className={"box2_in"} style={{width:width2+'px', height:height2+'px'}}>
-                </div>
-            </div>:''}
-            {big_box === 2?<div className={"box2_out"} style={{width:width2+'px', height:height2+'px'}}>
-                <div className={"box1_in"} style={{width:width1+'px', height:height1+'px'}}>
-                </div>
-            </div>:''}
+            <center>
+                {big_box === 1?<div className={"box1_out"} style={{width:width1+'px', height:height1+'px'}}>
+                    <div className={"box2_in"} style={{width:width2+'px', height:height2+'px'}}>
+                    </div>
+                </div>:''}
+                {big_box === 2?<div className={"box2_out"} style={{width:width2+'px', height:height2+'px'}}>
+                    <div className={"box1_in"} style={{width:width1+'px', height:height1+'px'}}>
+                    </div>
+                </div>:''}
+            </center>
         </>
     );
 }
